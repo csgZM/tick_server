d9188dae97017165358d9c57d92681496b0c3905
chenshuoguo <chenshuoguo@bytedance.com>
2024-07-04 10:32:31 +0800

    main

--- main.go
+++ main.go
@@ -18,14 +18,17 @@ type TestTickServiceModel struct {
 	TestServiceData string
 }
 
+// GetUniqueId 业务侧控制全局唯一计时器id
 func (t TestTickServiceModel) GetUniqueId() string {
 	return fmt.Sprintf("test_unique_%s", t.TestServiceData)
 }
 
+// OverDoFunc 计时器计时完成后的业务逻辑
 func (t TestTickServiceModel) OverDoFunc() {
 	fmt.Println("TestTickServiceModel over do function", t.TestServiceData)
 }
 
+// StopDoFunc 业务侧终止计时器后的业务逻辑
 func (t TestTickServiceModel) StopDoFunc() {
 	fmt.Println("TestTickServiceModel stop do function", t.TestServiceData)
 }
